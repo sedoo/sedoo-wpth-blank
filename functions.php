f7fb1fe3555afe20f3a34a7af62bb31abdee17ba
Pierre Vert <pierr65@gmail.com>
2023-01-13 15:41:20 +0100

    Get ready to nothing !

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * sedoo-wpth-blank , nothing inside !
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage sedoo-wpth-blank
+ * @since 1.0
+ */
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+// function sedoo-wpth-blank_enqueue() {
+// 	wp_enqueue_style( 'sedoo-wpth-blank-styles', get_stylesheet_uri() );
+// }
+// add_action( 'wp_enqueue_scripts', 'sedoo-wpth-blank_enqueue', 11 );
+
